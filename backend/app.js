25300fc1543aab73d0167b6a568d8bfd1988e6f0
RAED HOUIMLI <houimliraed@outlook.fr>
2023-12-21 17:28:30 +0100

    database connected

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,21 @@
-console.log("hello world !");
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use("/api",(req,res,next)=>{
+    res.send("hello world !");
+})
+
+mongoose.connect('mongodb+srv://admin:tYujSyJGEGXKSaAu@cluster0.injwsdh.mongodb.net/blogapp?retryWrites=true&w=majority').then(app.listen(5000,()=>{
+    console.log("application connected to the database and running on port 5000 ");
+})).catch(()=>{
+    console.log("error connecting to the database");
+})
+
+    
+
+
+
+
+//tYujSyJGEGXKSaAu
